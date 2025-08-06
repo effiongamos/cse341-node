83744db2425f7947ff77425bf1d267e3ae251b8c
effiongamos <beffiong3@byupathway.edu>
2025-08-06 20:18:47 +0100

    cse341-node

--- models/user.js
+++ models/user.js
@@ -1,16 +1,11 @@
+// models/user.js
+const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true  
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-  
-});
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'guest', 'admin'], default: 'user' },
+}, { timestamps: true });
+
+module.exports = mongoose.model('user', userSchema);
