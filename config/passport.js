304f3fba23d238b78e78add59fb6f2a98ea37d30
effiongamos <beffiong3@byupathway.edu>
2025-08-06 01:30:10 +0100

    cse341-node

--- config/passport.js
+++ config/passport.js
@@ -1,33 +1,18 @@
-const passport = require('passport');
+require('dotenv').config(); // Make sure this is at the top
+
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const passport = require('passport');
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    let user = await User.findOne({ googleId: profile.id });
-
-    if (!user) {
-      user = await User.create({
-        googleId: profile.id,
-        email: profile.emails[0].value,
-        name: profile.displayName,
-      });
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      // Your user logic here
+      return done(null, profile);
     }
-
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    user.token = token;
-    return done(null, user);
-  } catch (err) {
-    return done(err, null);
-  }
-}));
-
-passport.serializeUser((user, done) => done(null, user.id));
-passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => done(null, user));
-});
+  )
+);
