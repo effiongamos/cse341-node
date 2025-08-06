968d622691627e83b7a011892350bd2479a61a59
effiongamos <beffiong3@byupathway.edu>
2025-08-06 19:04:22 +0100

    cse341-node

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/usercontroller');
 const { verifyToken } = require('../middleware/authMiddleware'); 
 
 router.post('/register', userController.register);
